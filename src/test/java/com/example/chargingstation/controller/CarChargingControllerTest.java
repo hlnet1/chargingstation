bf30a7249c67408d98bbea8c5b58f1b6a7bd8ace
hlnet1 <hlnet@students.softuni.bg>
2024-03-17 18:45:12 +0200

    test fix

--- src/test/java/com/example/chargingstation/controller/CarChargingControllerTest.java
+++ src/test/java/com/example/chargingstation/controller/CarChargingControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -24,8 +25,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+
+@WebMvcTest(CarChargingController.class)
 class CarChargingControllerTest {
 
     private static final String BASE_URL = "http://localhost:8080/stations/";
